66b1f556c3bc6278109e21ae80f8e769707337a4
hwani <kyounghwan.choi@streami.co>
2021-07-25 21:02:08 +0900

    update

--- routers/router.go
+++ routers/router.go
@@ -1,14 +1,15 @@
 package routers
 
 import (
+	"github.com/dnfwlq8054/KO-Dev/routers/api/qna"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
-	apiv1 := router.Group("/api/qna")
+	//router.Group("/api/qna")
 
-	apiv1.GET("/qna/all", qna.all)
+	router.GET("/qna/all", qna.GetAll)
 
 	return router
 }
